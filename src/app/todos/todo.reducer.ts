a4ecebbdc30392740706314258c4652b5d3d4020
martinebucarey <martinebucarey@gmail.com>
2023-04-12 10:06:16 -0300

    add reactive forms

---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,14 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { Todo } from './models/todo.model';
+import { crear } from './todo.actions';
+
+export const estadoInicial: Todo[] = [new Todo('Salvar al mundo')];
+
+const _todoReducer = createReducer(
+  estadoInicial,
+  on(crear, (state, { texto }) => [...state, new Todo(texto)])
+);
+
+export function todoReducer(state: any, actions: Action) {
+  return _todoReducer(state, actions);
+}
