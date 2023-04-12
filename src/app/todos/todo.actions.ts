325f0175b038a60070449e2ccbba8a3616b6c76e
martinebucarey <martinebucarey@gmail.com>
2023-04-12 16:03:28 -0300

    add borrar

--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -9,3 +9,13 @@ export const toggle = createAction(
   '[TODO] Toggle Todo',
   props<{ id: number }>()
 );
+
+export const editar = createAction(
+  '[TODO] Editar Todo',
+  props<{ id: number; texto: string }>()
+);
+
+export const borrar = createAction(
+  '[TODO] Borrar Todo',
+  props<{ id: number }>()
+);
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,6 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { Todo } from './models/todo.model';
-import { crear, toggle } from './todo.actions';
+import { borrar, crear, editar, toggle } from './todo.actions';
 
 export const estadoInicial: Todo[] = [
   new Todo('Salvar al mundo'),
@@ -12,12 +12,28 @@ export const estadoInicial: Todo[] = [
 const _todoReducer = createReducer(
   estadoInicial,
   on(crear, (state, { texto }) => [...state, new Todo(texto)]),
+
+  on(borrar, (state, { id }) => state.filter((todo) => todo.id !== id)),
+
   on(toggle, (state, { id }) => {
     return state.map((todo) => {
       if (todo.id === id) {
         return {
           ...todo,
           completado: !todo.completado,
+        };
+      } else {
+        return todo;
+      }
+    });
+  }),
+
+  on(editar, (state, { id, texto }) => {
+    return state.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          texto: texto,
         };
       } else {
         return todo;
